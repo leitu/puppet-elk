a3978f7d3173250a94614f4a66809d8d3ed19400
Lei Tu <alex.lei.tu@gmail.com>
2015-01-05 14:20:02 +0800

    add key settings

--- manifests/server/config.pp
+++ manifests/server/config.pp
@@ -4,4 +4,14 @@ class elk::server::config {
     group  => $elk::params::group_name,
     source => "puppet:///modules/elk/elasticsearch.yml",
   }
+  file { '/etc/pki/tls/private/logstash-forwarder.key' :
+    owner  => $elk::params::user_name,
+    group  => $elk::params::group_name,
+    source => "puppet:///modules/elk/logstash-forwarder.key",
+  }
+  file { '/etc/pki/tls/certs/logstash-forwarder.crt' :
+    owner  => $elk::params::user_name,
+    group  => $elk::params::group_name,
+    source => "puppet:///modules/elk/logstash-forwarder.crt",
+  }
 }
